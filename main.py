b926eeec7fe5f2623196f99108571d18cae91453
Diego Vega <davegav@unal.edu.co>
2020-12-10 15:27:31 -0500

    Conectar con heroku

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
     "http://localhost", "http://localhost:8080",
+    "https://vistas-cajero.herokuapp.com"
     ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
